ed6659638c0f89d86c62dbf00ecf498f827e5e22
Minishxco <minishxco@gmail.com>
2026-02-11 17:56:23 -0600

    Seleccionar con Enter

--- Assets/Scripts/CharacterSelector.cs
+++ Assets/Scripts/CharacterSelector.cs
@@ -6,26 +6,90 @@ public class CharacterSelector : MonoBehaviour
     public GameObject character1Image;
     public GameObject character2Image;
 
-    private int selectedCharacter = 0;
+    private int selectedCharacter = 1; // 1 o 2
+    private int maxCharacters = 2;
 
     public VideoSelector videoSelector;
     public UserDataSaver dataSaver;
+    public GameObject panelSeccionar;
 
     private void Start()
     {
         SelectCharacter2();
     }
 
+    private void Update()
+    {
+        if (!panelSeccionar.activeSelf) return;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            NextCharacter();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            PreviousCharacter();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ConfirmSelectedCharacter();
+        }
+    }
+
+    public void NextCharacter()
+    {
+        selectedCharacter++;
+
+        if (selectedCharacter > maxCharacters)
+            selectedCharacter = 1;
+
+        UpdateSelection();
+    }
+
+    public void PreviousCharacter()
+    {
+        selectedCharacter--;
+
+        if (selectedCharacter < 1)
+            selectedCharacter = maxCharacters;
+
+        UpdateSelection();
+    }
+
+    private void UpdateSelection()
+    {
+        if (selectedCharacter == 1)
+            SelectCharacter1();
+        else
+            SelectCharacter2();
+    }
+
     public void SelectCharacter1()
     {
+        selectedCharacter = 1;
         character1Image.SetActive(true);
         character2Image.SetActive(false);
     }
 
     public void SelectCharacter2()
     {
+        selectedCharacter = 2;
         character2Image.SetActive(true);
         character1Image.SetActive(false);
+    }
+
+    private void ConfirmSelectedCharacter()
+    {
+        if (selectedCharacter == 1)
+        {
+            ConfirmCharacter1();
+        }
+        else
+        {
+            ConfirmCharacter2();
+        }
     }
 
     public void ConfirmCharacter1()
