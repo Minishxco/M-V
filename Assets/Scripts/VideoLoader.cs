b373c9082b607bd41d77869c626cb45f67d26085
Minishxco <minishxco@gmail.com>
2026-02-24 11:34:23 -0600

    Ajustes Pantalla

---
+++ Assets/Scripts/VideoLoader.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoLoader : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public string urlVideo = "Video/FONDO_COCHE_FUEGO.mp4";
+
+    void Start()
+    {
+        // Ruta al video en StreamingAssets
+        string path = System.IO.Path.Combine(Application.streamingAssetsPath, urlVideo);
+        videoPlayer.url = path;
+        videoPlayer.Play();
+    }
+}
