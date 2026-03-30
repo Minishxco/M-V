671bdbe36c358d8f8fa4bc2df3fdfabc442722b9
Minishxco <minishxco@gmail.com>
2026-03-30 11:34:35 -0600

    Últimas pruebas

--- Assets/Scripts/PrintScreen.cs
+++ Assets/Scripts/PrintScreen.cs
@@ -3,11 +3,13 @@ using System.IO;
 using System.Diagnostics;
 using System.Collections;
 using TMPro;
+using UnityEngine.UI;
 
 public class PrintScreen : MonoBehaviour
 {
     private string rutaTemporal;
     public TextMeshProUGUI textName;
+    public Button botonImprimir;
 
     private void Awake()
     {
@@ -19,27 +21,39 @@ public class PrintScreen : MonoBehaviour
 
     public void CapturarEImprimir()
     {
-        rutaTemporal = Path.Combine(Path.GetTempPath(), "captura_unity.jpg");
-        StartCoroutine(Capturar());
+        StartCoroutine(ProcesoCaptura());
+    }
+
+    private IEnumerator ProcesoCaptura()
+    {
+        if (botonImprimir != null)
+            botonImprimir.gameObject.SetActive(false);
+
+        yield return null;
+
+        yield return StartCoroutine(Capturar());
+
+        yield return new WaitForSeconds(1f);
+
+        if (botonImprimir != null)
+            botonImprimir.gameObject.SetActive(true);
     }
 
     private IEnumerator Capturar()
     {
         yield return new WaitForEndOfFrame();
 
-        // Captura
         Texture2D tex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
         tex.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
         tex.Apply();
 
-        // Guardar automática
+        rutaTemporal = Path.Combine(Path.GetTempPath(), "captura_unity.jpg");
         File.WriteAllBytes(rutaTemporal, tex.EncodeToJPG(100));
         Destroy(tex);
 
-        // Mandar a imprimir por Windows
         ProcessStartInfo psi = new ProcessStartInfo();
         psi.FileName = rutaTemporal;
-        psi.Verb = "print";     // <- imprime con impresora predeterminada
+        psi.Verb = "print";
         psi.CreateNoWindow = true;
         psi.WindowStyle = ProcessWindowStyle.Hidden;
 
