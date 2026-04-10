16e54d3c8911f21b9bb8490a1a6e551b5a05391d
FedericoVS <minishxco@gmail.com>
2026-04-09 21:44:11 -0600

    Video y texto

--- Assets/Scripts/VideoLoader.cs
+++ Assets/Scripts/VideoLoader.cs
@@ -4,15 +4,22 @@ using UnityEngine.Video;
 public class VideoLoader : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
-    public string urlVideo = "Video/FONDO_COCHE_FUEGO.mp4";
+    public string urlVideo = "Video/FONDO_COCHE_FUEGO.mov";
 
     void Start()
     {
-        // Ruta al video en StreamingAssets
         string path = System.IO.Path.Combine(Application.streamingAssetsPath, urlVideo);
         videoPlayer.url = path;
-        videoPlayer.Play();
 
-        Debug.Log(path);
+        videoPlayer.isLooping = false;
+        videoPlayer.loopPointReached += OnVideoEnd;
+
+        videoPlayer.Play();
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        vp.frame = 0;
+        vp.Play();
     }
 }
